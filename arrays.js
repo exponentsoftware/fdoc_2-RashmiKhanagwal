72d0e536bfbb3a9c4a76254e5cdd97c4746ce584
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-01-14 22:55:50 +0530

    array of arrays

---
+++ arrays.js
@@ -0,0 +1,48 @@
+// 1. Using the countries array create the following array of arrays. The country name, the first three letters of the country name and the length of the country name.
+
+// ```js
+const countries = [
+  'ALBANIA',
+  'BOLIVIA',
+  'CANADA',
+  'DENMARK',
+  'ETHIOPIA',
+  'FINLAND',
+  'GERMANY',
+  'HUNGARY',
+  'IRELAND',
+  'JAPAN',
+  'KENYA'
+]
+// ```
+
+// ```sh
+// createArrayOfArrays(countries)
+// [
+//   ['Albania', 'ALB', 7],
+//   ['Bolivia', 'BOL', 7],
+//   ['Canada', 'CAN', 6],
+//   ['Denmark', 'DEN', 7],
+//   ['Ethiopia', 'ETH', 8],
+//   ['Finland', 'FIN', 7],
+//   ['Germany', 'GER', 7],
+//   ['Hungary', 'HUN', 7],
+//   ['Ireland', 'IRE', 7],
+//   ['Japan', 'JAP', 5],
+//   ['Kenya', 'KEN', 5]
+// ]
+// ```
+
+let array = [];
+  createArrayOfArrays(countries);
+
+  function createArrayOfArrays(countries){
+       countries.map((item) => {
+           const start = item.split("").splice(0,3).join("");
+           const countrieslength = item.length;
+
+           return array.push([item + "" + ", " + start + "" + ", " + countrieslength])
+       });
+}
+console.log(array);
+
